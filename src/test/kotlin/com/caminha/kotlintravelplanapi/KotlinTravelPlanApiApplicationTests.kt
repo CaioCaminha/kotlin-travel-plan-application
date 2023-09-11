bb35152cd92dae3237989cfc47d808b174e2f132
Caio Caminha <caio.caminha@globalpay.com>
2023-09-11 17:38:26 -0300

    initial commit - creating basic structure of the project

---
+++ src/test/kotlin/com/caminha/kotlintravelplanapi/KotlinTravelPlanApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caminha.kotlintravelplanapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTravelPlanApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
